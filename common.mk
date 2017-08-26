c83b4e18fb4db5a7500936d6d874fd1aee5fd727
Seraph08 <d-vandenburgh@hvcc.edu>
2017-08-26 17:34:53 -0400

    merge CAF tag LA.UM.5.8.r1-02700-8x98

--- common.mk
+++ common.mk
@@ -1,4 +1,7 @@
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+# Include versioning information 
+# Format: Major.minor.maintenance(-TAG) 
+export CAF_VERSION := LA.UM.5.8.r1-02700-8x98.0
 
 PRODUCT_PROPERTY_OVERRIDES += \
     keyguard.no_require_sim=true \
