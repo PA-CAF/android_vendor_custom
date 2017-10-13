e44ee354e4ebb7eb8572bb6df252c1953121ec2e
David Vandenburgh <d-vandenburgh@hvcc.edu>
2017-10-13 14:58:36 -0400

    vendor: CAF build tag LA.UM.5.8.r1-02900-8x98.0

--- common.mk
+++ common.mk
@@ -2,7 +2,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 # Include versioning information 
 # Format: Major.minor.maintenance(-TAG) 
-export CAF_VERSION := LA.UM.5.8.r1-02800-8x98.0
+export CAF_VERSION := LA.UM.5.8.r1-02900-8x98.0
 
 PRODUCT_PROPERTY_OVERRIDES += \
     keyguard.no_require_sim=true \
