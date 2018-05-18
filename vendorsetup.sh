aa03cab0cda3b1fecdef58dcbae28c1c308b56a1
Seraph08 <vandend278@cobleskill.edu>
2018-05-18 01:43:24 -0400

    clean up vendor just for sdclang

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,37 +1,4 @@
-# CAF
-add_lunch_combo pa_bacon-userdebug
-add_lunch_combo pa_gemini-userdebug
-add_lunch_combo pa_oneplus2-userdebug
-add_lunch_combo pa_oneplus3-userdebug
-add_lunch_combo pa_onyx-userdebug
-add_lunch_combo pa_r7plus-userdebug
-add_lunch_combo pa_robin-userdebug
-add_lunch_combo pa_trltexx-userdebug
-add_lunch_combo pa_titan-userdebug
-add_lunch_combo pa_zl1-userdebug
-
-# Samsung Exynos devices
-add_lunch_combo pa_gts210ltexx-userdebug
-add_lunch_combo pa_gts210wifi-userdebug
-
-# AOSP
-add_lunch_combo pa_angler-userdebug
-add_lunch_combo pa_bullhead-userdebug
-add_lunch_combo pa_hammerhead-userdebug
-add_lunch_combo pa_marlin-userdebug
-add_lunch_combo pa_sailfish-userdebug
-add_lunch_combo pa_shamu-userdebug
-
-# SONY OpenDevices
-# Loire
-add_lunch_combo pa_kugo-userdebug
-add_lunch_combo pa_suzu-userdebug
-# Tone
-add_lunch_combo pa_dora-userdebug
-add_lunch_combo pa_kagura-userdebug
-add_lunch_combo pa_keyaki-userdebug
-
 # SDClang Environment Variables
-export SDCLANG_AE_CONFIG=vendor/pa/sdclang/sdclangAE.json
-export SDCLANG_CONFIG=vendor/pa/sdclang/sdclang.json
+export SDCLANG_AE_CONFIG=vendor/custom/sdclang/sdclangAE.json
+export SDCLANG_CONFIG=vendor/custom/sdclang/sdclang.json
 export SDCLANG_SA_ENABLED=false
